4247e384373337f3c7fc0407733bce91420d7be2
jeremyvw <jeremyvw30@gmail.com>
2020-04-27 15:38:02 +0700

    Decorating Login Page

--- app/cache/_mnt_d_tc its_git_phalcon_colimas_app_views_user_login.volt.php
+++ app/cache/_mnt_d_tc its_git_phalcon_colimas_app_views_user_login.volt.php
@@ -13,23 +13,23 @@
             <div class="container-login100">
                 <div class="wrap-login100">
                     <form class="login100-form validate-form" method="POST" autocomplete="off" action="<?= $this->url->get('/session/login') ?>">
-                        <span class="login100-from-title">Login</span>
+                        <span class="login100-form-title">Login</span><br>
                         <div class="wrap-input100 validate-input">
                             <label for="email">E-mail</label>
-                            <input class="input100" type="email" name="email" placeholder="123@example.com" required>
+                            <input class="input10" type="email" name="email" placeholder="123@example.com" required>
                             <span class="symbol-input100"><i class="fa fa-envelope" aria-hidden="true"></i></span>
                         </div>
                         <div class="wrap-input100 validate-input">
                             <label for="password">Password</label>
-                            <input class="input100" type="password" name="password" required>
+                            <input class="input10" type="password" name="password" required>
                             <span class="symbol-input100"><i class="fa fa-lock" aria-hidden="true"></i></span>
                         </div>
                         <div class="container-login100-form-btn">
                             <button type="submit" class="login100-form-btn">Sign In</button>
                         </div>
                         <div class="text-center p-t-23 p-b-70">
-                            <span class="txt1">Don't have an account yet?</span>
-                                <a class="txt2" href="<?= $this->url->get('auth/register') ?>">
+                            <span class="txt3">Don't have an account yet?</span>
+                                <a class="txt3" href="<?= $this->url->get('user/register') ?>">
                                     Create your account
                                     <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
                                 </a>
